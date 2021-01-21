4dcd5634d34ac42cde2e7f640aada8f83fa6c356
Maxim Dubrovin <max28101998@gmail.com>
2021-01-21 17:25:52 +0300

    created todo app

---
+++ src/containers/todo/todo.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addTask, removeTask, completeTask, changeFilter } from '../../actions/actionCreator';
+
+import ToDoInput from '../../components/todo-input/todo-input';
+import ToDoList from '../../components/todo-list/todo-list';
+import Footer from '../../components/footer/footer';
+
+import './todo.sass';
+
+class ToDo extends Component {
+
+  state = {
+    taskText: ''
+  }
+
+  handlerInputChange = ({ target: { value } }) => {
+    this.setState({
+      taskText: value,
+    })
+  }
+
+  addTask = ({ key }) => {
+    const { taskText } = this.state;
+
+    if (taskText.length > 3 && key === 'Enter') {
+      const { addTask } = this.props;
+
+      addTask((new Date()).getTime(), taskText, false);
+
+      this.setState({
+        taskText: ''
+      })
+    }
+  }
+
+  filterTasks = (tasks, activeFilter) => {
+    switch (activeFilter) {
+      case 'completed':
+        return tasks.filter(task => task.isCompleted);
+
+      case 'active':
+        return tasks.filter(task => !task.isCompleted);
+
+      default:
+        return tasks;
+    }
+  }
+
+  getActiveTasksCounter = tasks => tasks.filter(task => !task.isCompleted).length;
+
+  render() {
+    const { taskText } = this.state;
+    const { tasks, removeTask, completeTask, filters, changeFilter } = this.props;
+    const isTasksExist = tasks && tasks.length > 0;
+    const filteredTasks = this.filterTasks(tasks, filters);
+    const taskCounter = this.getActiveTasksCounter(tasks);
+
+    return (
+      <div className="todo-wrapper">
+        <ToDoInput onKeyPress={this.addTask} onChange={this.handlerInputChange} value={taskText} />
+        {isTasksExist && <ToDoList tasksList={filteredTasks} removeTask={removeTask} completeTask={completeTask} />}
+        {isTasksExist && <Footer changeFilter={changeFilter} amount={taskCounter} activeFilter={filters} />}
+      </div>
+    );
+  }
+}
+
+export default connect(({ tasks, filters }) => ({
+  tasks,
+  filters,
+}), { addTask, removeTask, completeTask, changeFilter })(ToDo);
---
+++ src/reducers/tasks.js
@@ -0,0 +1,58 @@
+import { ADD_TASK, REMOVE_TASK, COMPLETE_TASK } from '../constants';
+import { load } from 'redux-localstorage-simple';
+
+
+let TASKS = load({ namespace: 'todo-app' });
+
+if (!TASKS || !TASKS.tasks || !TASKS.tasks.length) {
+  TASKS = {
+    tasks: [],
+  }
+}
+
+// const TASKS = [
+//   {
+//     id: 1,
+//     text: 'Learn ReactJS',
+//     isCompleted: true,
+//   },
+//   {
+//     id: 2,
+//     text: 'Learn Redux',
+//     isCompleted: false,
+//   },
+//   {
+//     id: 3,
+//     text: 'Learn React Router',
+//     isCompleted: false,
+//   }
+// ];
+
+const tasks = (state = TASKS.tasks, { type, id, text, isCompleted }) => {
+  switch (type) {
+    case ADD_TASK:
+      return [
+        ...state, {
+          id,
+          text,
+          isCompleted
+        }
+      ];
+
+    case REMOVE_TASK:
+      return [...state].filter(task => task.id !== id);
+
+    case COMPLETE_TASK:
+      return [...state].map(task => {
+        if (task.id === id) {
+          task.isCompleted = !task.isCompleted;
+        }
+        return task;
+      });
+
+    default:
+      return state;
+  }
+}
+
+export default tasks;
